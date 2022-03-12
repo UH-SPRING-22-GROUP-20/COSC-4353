fc7a1cd1c35f56626db564bfbb407c0ad60c2949
MeL <90160548+melyespinofl96@users.noreply.github.com>
2022-03-11 18:58:30 -0600

    login module finished i think lol

--- main.js
+++ main.js
@@ -21,21 +21,46 @@ async function openTab(evt, Tab) {
 // ------------------ function for tabs ends ---------------
 // ------------------ function to validate login and register ---------------
 
+/*
 async function validate_login(valid) {
-
-    if (valid) {
+    if (valid){
         location.href = 'contents/dashboard.html';
     }
-
 }
 
 async function validate_reg(valid) {
-
     if (valid) {
         location.href = 'contents/dashboard.html';
         alert("Thank you for making an account with us, \ngo ahead and update your profile");
     }
+}
+*/
 
+async function validate_login() {
+    let x = document.getElementById("signin-user").value;
+    let y = document.getElementById("signin-pass").value;
+
+    if(isNaN(x.length) || x.length < 1 || x.length > 10)
+        alert("Username not valid");
+    else if(isNaN(y.length) || y.length < 1 || y.length > 10)
+        alert("Password not valid");
+    else
+        location.href = 'contents/dashboard.html';
+}
+
+async function validate_reg() {
+    let x = document.getElementById("signup-user").value;
+    let y = document.getElementById("signup-pass").value;
+    let z = document.getElementById("signup-repeat").value;
+
+    if(isNaN(x.length) || x.length < 1 || x.length > 10)
+        alert("Username not valid");
+    else if(isNaN(y.length) || y.length < 1 || y.length > 10)
+        alert("Password not valid");
+    else if(y.localeCompare(z))
+        alert("Passwords are not the same");
+    else
+        location.href = 'contents/dashboard.html';
 }
 
 async function logout(){
